871a4a66825e0a38ce119ae3ea7e80b9fe1cbda3
LALA <you@gmail.com>
2024-11-14 20:43:25 +0300

    AHHA

--- main.cpp
+++ main.cpp
@@ -1,28 +1,7 @@
-#define <iostream>
-
-using namespace std;
+#include "menu.h"
 
 int main(){
-while (true){
-cout << "1.Menu: " << endl;
-cout << "2.Option 1: " << endl;
-cout << "3.Option 2: " << endl;
-cout << "4.Option 3: " << endl;
-cout << "5.Exit: " << endl;
+displayMenu();
 }
 
-switch (choice){
-case 1:
-     cout << "Option 1" << endl;
-     break;
-case 2:
-     cout << "Option 2" << endl;
-     break;
-case 3:
-     cout << "Option 3" << endl;
-     break;
-case 4:
-      cout << "Exiting ......" << endl;
-      return 0;
-}
-}
+
---
+++ menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <iostream>
+
+void displayMenu();
+
+#endif // MENU_H
